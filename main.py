8330c509fab33d14bc1863de4088accabfcabf2a
Arnold <k_arnold95@hotmail.com>
2022-11-10 15:52:19 +0000

    Implemented code to show how the classes works with examples

--- main.py
+++ main.py
@@ -4,8 +4,32 @@ from algConverter import AlgConverter
 from timer import Timer
 
 def main():
-    #Program starts here!
-    pass
+    #Using timer example:
+    my_timer = Timer()
+    input('Press any key to start timer!')
+    my_timer.start()
+    input('Press any key to stop timer!')
+    solved_time = my_timer.stop()
+    print(solved_time)
+
+    #Using cubeSolver example
+    solver = CubeSolver()
+    solution = solver.solve('DRLUUBFBRBLURRLRUBLRDDFDLFUFUFFDBRDUBRUFLLFDDBFLUBLRBD')
+    print(solution)
+
+    #Using algConverter example
+    converter = AlgConverter()
+    converted_moves_one = converter.convert_to_u('U')
+    print(converted_moves_one)
+    converter.clear_converted_moves()
+
+    converted_moves_two = converter.convert_to_u('U\'')
+    print(converted_moves_two)
+    converter.clear_converted_moves()
+
+    converted_moves_three = converter.convert_to_u('U2')
+    print(converted_moves_three)
+    converter.clear_converted_moves()
 
 
 if __name__ == '__main__':
