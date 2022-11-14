fa3c480e11d97748d1de87d29696810837910b72
Arnold <k_arnold95@hotmail.com>
2022-11-14 21:31:43 +0000

    Added ColorSideMapper class and added slicing to main (is out commented)

---
+++ colorSideMapper.py
@@ -0,0 +1,16 @@
+class ColorSideMapper:
+    def __init__(self) -> None:
+        self.opposite_color: dict[str, str] = {'W': 'Y', 'Y': 'W', 'R': 'O', 'O': 'R', 'B': 'G', 'G': 'B'}
+        self.opposite_side: dict[str, str] = {'U': 'D', 'D': 'U', 'L': 'R', 'R': 'L', 'F': 'B', 'B': 'F'}
+
+    def get_opposite_color(self, color: str) -> str:
+        '''Takes in a color on the cube.
+        Returns the opposite color
+        '''
+        return self.opposite_color[color]
+
+    def get_opposite_side(self, side: str) -> str:
+        '''Takes in a side on the cube.
+        Returns the opposite side
+        '''
+        return self.opposite_side[side]
--- main.py
+++ main.py
@@ -15,8 +15,20 @@ def main():
     """
     #Using cubeSolver example
     solver = CubeSolver()
-    solution = solver.solve('DRLUUBFBRBLURRLRUBLRDDFDLFUFUFFDBRDUBRUFLLFDDBFLUBLRBD')
+    
+    #Order is U, D, F, R, B, L (easier to follow than the kociemba library order)
+    cube = ('LBBUUDDDULFULDFLUBFRFBFLBDBRBLFRLRRDURFBBDRFFURRLLUDUD')
+    #Change order to what the kociemla library accepts (which is U, R, F, D, L, B)
+    cube = cube[0:9] + cube[27:36] + cube[18:27] + cube[9:18] + cube[45:54] + cube[36:45]
+    
+    solution = solver.solve(cube)
     print(solution)
+    #TODO
+    #When the user will enter the cube as imput, we want to do something like the following:
+    #ask user to enter which side is top and which is front
+    #tell user (with picture maybe) to order from top -> bottom/down -> front -> right -> back -> left
+    #make it such that the input color buttons tell the program the right side
+    #e.g. the user says - top: white, front: red, right: blue, then make the white button give 'U' to the program, red gives 'F', yellow gives 'D' and so on
     """
 
     #Using algConverter example
