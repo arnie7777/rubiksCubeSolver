5f6993a6cff548cee871898a1be92e555d7ce85f
Arnold <k_arnold95@hotmail.com>
2022-11-19 22:38:49 +0000

    Testing frame in gui

--- gui.py
+++ gui.py
@@ -6,26 +6,29 @@ class GUI:
         
 
     def __start_screen_setup(self):
+        # create root widget and title
         self.root = Tk()
         self.root.title('Rubiks cube solver')
 
-        self.top_color_frame = Frame(self.root).grid(row=0, column=0)
+        # create a frame in root
+        self.top_color_frame = Frame(self.root).pack(padx=10, pady=10, side='bottom')
         
-        self.top_color_label = Label(self.top_color_frame, text='Enter top center color of the cube')
-        self.top_color_label.grid(row=0, column=0, columnspan=7)
+        # create top label in the frame
+        self.top_color_label = Label(self.top_color_frame, text='Enter top center color of the cube').pack()
 
+        # create empty label where the chosen color by the user will be displayed
         self.top_color_entry = Label(self.top_color_frame)
-        self.top_color_entry.grid(row=1, column=0, columnspan=7)
+        self.top_color_entry.pack()
 
-        Button(self.top_color_frame, text='White', command=lambda: self.__top_color_entered('White')).grid(row=2, column=0)
-        Button(self.top_color_frame, text='Yellow', command=lambda: self.__top_color_entered('Yellow')).grid(row=2, column=1)
-        Button(self.top_color_frame, text='Red', command=lambda: self.__top_color_entered('Red')).grid(row=2, column=4)
-        Button(self.top_color_frame, text='Orange', command=lambda: self.__top_color_entered('Orange')).grid(row=2, column=5)
-        Button(self.top_color_frame, text='Blue', command=lambda: self.__top_color_entered('Blue')).grid(row=2, column=2)
-        Button(self.top_color_frame, text='Green', command=lambda: self.__top_color_entered('Green')).grid(row=2, column=3)
+        Button(self.top_color_frame, text='White', command=lambda: self.__top_color_entered('White')).pack()
+        Button(self.top_color_frame, text='Yellow', command=lambda: self.__top_color_entered('Yellow')).pack()
+        Button(self.top_color_frame, text='Red', command=lambda: self.__top_color_entered('Red')).pack()
+        Button(self.top_color_frame, text='Orange', command=lambda: self.__top_color_entered('Orange')).pack()
+        Button(self.top_color_frame, text='Blue', command=lambda: self.__top_color_entered('Blue')).pack()
+        Button(self.top_color_frame, text='Green', command=lambda: self.__top_color_entered('Green')).pack()
 
-        self.top_color_frame = Frame(self.root).grid(row=0, column=0)
-        
+        self.top_color_frame = Frame(self.root).pack()
+
         self.root.mainloop()
 
 
